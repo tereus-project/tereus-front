6ed49e378ddcb97799e09b7886fee87ecf09e31f
quantumsheep <nathanael.dmc@outlook.fr>
2022-04-22 14:14:58 +0200

    Handle dev login

--- app/routes/login.tsx
+++ app/routes/login.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import { LoaderFunction, useLoaderData } from "remix";
 import { Page } from "~/components/Page";
 
-export const loader: LoaderFunction = async () => {
+export const loader: LoaderFunction = async ({ request }) => {
+  let githubLoginUrl = `https://github.com/login/oauth/authorize?scope=user:email%20repo%20gist&client_id=${process.env.GITHUB_OAUTH2_CLIENT_ID}`;
+
+  if (process.env.NODE_ENV === "development") {
+    const url = new URL(request.url);
+    githubLoginUrl += `&redirect_uri=http://127.0.0.1:${url.port}/auth/github`;
+  }
+
   return {
-    githubLoginUrl: `https://github.com/login/oauth/authorize?scope=user:email%20repo%20gist&client_id=${process.env.GITHUB_OAUTH2_CLIENT_ID}`,
+    githubLoginUrl,
   };
 }
 
