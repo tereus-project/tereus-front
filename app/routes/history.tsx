38020ebae98c366960aa7559b28d84612507f971
quantumsheep <nathanael.dmc@outlook.fr>
2022-04-28 22:21:50 +0200

    Improve history page typing

--- app/routes/history.tsx
+++ app/routes/history.tsx
@@ -1,12 +1,16 @@
 import { EuiBasicTable } from "@elastic/eui";
 import { formatRelative, parseJSON } from "date-fns";
-import React, { useEffect, useState } from "react";
-import { json, LoaderFunction, redirect, useActionData, useLoaderData, useOutletContext, useTransition } from "remix";
+import React, { useState } from "react";
+import { json, LoaderFunction, redirect, useLoaderData, useOutletContext } from "remix";
 import * as api from "~/api";
-import { ActionFormData } from "~/api";
 import { Page } from "~/components/Page";
 import { sessionCookie } from "~/cookie";
 import { TereusContext } from "~/root";
+
+interface LoaderResponse {
+  response?: api.SubmissionDTO[];
+  errors: string[] | null;
+}
 
 export const loader: LoaderFunction = async ({ request }) => {
   const cookieHeader = request.headers.get("Cookie");
@@ -17,27 +21,14 @@ export const loader: LoaderFunction = async ({ request }) => {
   }
 
   const [response, errors] = await api.getUserSubmissions(session.token);
-  console.log(response?.submissions);
   return json({ response: response?.submissions, errors });
 };
 
 export default function History() {
   const context = useOutletContext<TereusContext>();
+  const loaderData = useLoaderData<LoaderResponse>();
 
-  const transition = useTransition();
-
-  const actionData = useActionData<ActionFormData<api.RemixResponseDTO>>();
-  const loaderData = useLoaderData<Awaited<ReturnType<typeof loader>>>();
-
-  const [sources, setSources] = useState(loaderData.response);
-
-  useEffect(() => {
-    if (transition.state === "loading" && actionData?.response) {
-      console.log(actionData);
-      setSources(sources.concat(actionData.response));
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [transition]);
+  const [sources] = useState(loaderData.response ?? []);
 
   return (
     <Page title="Remix history" icon="clock" user={context.user}>
