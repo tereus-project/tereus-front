2a44b6a29b34ec38579e623c4be4b6e0ed15b1b7
quantumsheep <nathanael.dmc@outlook.fr>
2022-05-08 17:06:00 +0200

    Fix formatting

--- app/components/NavBarGroup.tsx
+++ app/components/NavBarGroup.tsx
@@ -3,9 +3,5 @@ import { Flex } from "@chakra-ui/react";
 export type PageProps = React.PropsWithChildren<{}>;
 
 export function NavBarGroup({ children }: PageProps) {
-  return (
-    <Flex align="center">
-      {children}
-    </Flex>
-  );
+  return <Flex align="center">{children}</Flex>;
 }
