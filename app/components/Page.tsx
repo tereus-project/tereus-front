42d4b5c8b274a810689bec47cc38a83ffb9017ea
Stanislas Lange <git@slange.me>
2022-07-01 22:28:46 +0000

    Improve title style for login

--- app/components/Page.tsx
+++ app/components/Page.tsx
@@ -15,11 +15,11 @@ export function Page({ children, title, subtitle, containerSize, containerFluid,
     <main>
       <Container size={containerSize} fluid={containerFluid} mb={18}>
         <Container size={headerSize ?? containerSize} px={0} fluid={headerFluid}>
-          <Stack mb={14} spacing={2}>
+          <Stack mb={14} spacing={20}>
             <Title order={1}>{title}</Title>
             {subtitle && (
               <Title
-                order={2}
+                order={4}
                 sx={(theme) => ({
                   color: theme.colors.gray[5],
                 })}
